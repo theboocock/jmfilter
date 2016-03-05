a6f4aef5773bee52aa071875d8b451cddebf628a
James Boocock <james.boocock@otago.ac.nz>
2016-03-05 15:38:12 +1300

    Updated loc filter to output based on a command-line args.
    Also, made sure to ignore output files in the scripts directory

--- loc_filter.py
+++ loc_filter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import argparse
 import logging
+import sys
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
 class LocFile(object):
@@ -83,7 +84,7 @@ def remove_bad_call_rate(loc_file, missing):
 
     logging.info("Remove sites that have a bad call rate")
     lines = []
-    call_rate_thresh = missing 
+    call_rate_thresh = missing
     for i, line in enumerate(loc_file.lines):
         split_line = line.split("\t")
         id = split_line[0]
@@ -199,19 +200,16 @@ def check_impossible_genotypes(loc_file,f2_pvalue, backcross_pvalue):
                 lines.append(line)
     loc_file.update_lines(lines)
 
-def output_loc_map_loc_files(loc_file):
+def output_loc_map_loc_files(loc_file, output_file):
     """
         Output locus map file
     """
-    output_map = "output_map.map"
-    output_loc = "output_loc.loc"
-    output_map_f = open(output_map, 'w')
+    output_loc = output_file 
     output_loc_f = open(output_loc, 'w')
     output_loc_f.write(loc_file.get_header())
     for line in loc_file.lines:
         output_loc_f.write(line + "\n")
         split_line = line.split('\t')
-        output_map_f.write('\t'.join([split_line[0],split_line[1]]) + '\n')
 
 def main():
     """
@@ -224,14 +222,22 @@ def main():
                         help="Segregant Chi-square P-value filter for maternally and paternally"
                         "informative back crosses: 1:1 expected ratio (default = 0.0001)", default=0.0001)
     parser.add_argument("-f", "--f2-pvalue", dest="f2_pvalue", help="Segregant Chi-square P-value"
-                        "filter for`f2 crosses: 1:2:1 expected ratio (default = 0.001)")
+                        "filter for`f2 crosses: 1:2:1 expected ratio (default = 0.001)", default=0.001)
+    parser.add_argument("-o", "--output_file", dest="output_file", help="Output locus file" ,default="output_loc.loc")  
     logging.info("Started LOC filter for linkage map creation")
     args = parser.parse_args()
+    try: 
+        args.backcross_pvalue = float(args.backcross_pvalue)
+        args.missing = float(args.missing)
+        args.f2_pvalue = float(args.f2_pvalue)
+    except:
+        logging.error("Could not convert number arguments please check you command-line arguments")
+        sys.exit(1)
     loc_file = args.loc_file
     loc_file = LocFile(loc_file)
     remove_missing_parent_markers(loc_file)
     remove_bad_call_rate(loc_file, args.missing)
     check_impossible_genotypes(loc_file, args.f2_pvalue,args.backcross_pvalue)
-    output_loc_map_loc_files(loc_file)
+    output_loc_map_loc_files(loc_file, args.output_file)
 if __name__ == "__main__":
     main()
